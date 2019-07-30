6453cf48998fb11441052d3aa2141ce05d2e24a9
Aroa GB <aroa@framer.com>
2019-07-30 18:18:37 +0200

    Complete components: RadioButton, RangeInput and Checkbox

--- code/CheckBox.tsx
+++ code/CheckBox.tsx
@@ -1,26 +1,59 @@
-import * as React from "react";
-import * as System from "grommet";
-import { ControlType, PropertyControls, addPropertyControls } from "framer";
-import { controls, merge } from "./inferredProps/CheckBox";
+import * as React from "react"
+import * as System from "grommet"
+import { ControlType, PropertyControls, addPropertyControls } from "framer"
+import { controls, merge } from "./inferredProps/CheckBox"
 
 const style: React.CSSProperties = {
-  width: "100%",
-  height: "100%"
-};
+    width: "100%",
+    height: "100%",
+}
 
 export function CheckBox(props) {
-  return <System.CheckBox {...props} style={style} />;
+    const {
+        checked,
+        color,
+        disabled,
+        onChange,
+        indeterminate,
+        label,
+        ["children"]: _,
+        ...rest
+    } = props
+
+    const [state, setState] = React.useState({ checked })
+
+    React.useEffect(() => {
+        setState({ checked })
+    }, [checked])
+
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        onChange && onChange(event)
+        setState({ ...state, checked: event.target.checked })
+    }
+    return (
+        <System.CheckBox
+            {...rest}
+            style={style}
+            checked={state.checked}
+            onChange={handleChange}
+            disabled={props.disabled}
+            label={props.label}
+        />
+    )
 }
 
 CheckBox.defaultProps = {
-  width: 150,
-  height: 50
-};
+    width: 100,
+    height: 24,
+    disabled: false,
+    checked: false,
+    label: "Hello World",
+}
 
 addPropertyControls(CheckBox, {
-  label: merge(controls.label, {}),
-  reverse: merge(controls.reverse, {}),
-  toggle: merge(controls.toggle, {}),
-  indeterminate: merge(controls.indeterminate, {}),
-  placeholder: merge(controls.placeholder, {})
-});
+    label: merge(controls.label, {}),
+    checked: merge(controls.checked, {}),
+    disabled: merge(controls.disabled, {}),
+    reverse: merge(controls.reverse, {}),
+    toggle: merge(controls.toggle, {}),
+})
--- code/RadioButton.tsx
+++ code/RadioButton.tsx
@@ -1,23 +1,59 @@
-import * as React from "react";
-import * as System from "grommet";
-import { ControlType, PropertyControls, addPropertyControls } from "framer";
-import { controls, merge } from "./inferredProps/RadioButton";
+import * as React from "react"
+import * as System from "grommet"
+import { ControlType, PropertyControls, addPropertyControls } from "framer"
+import { controls, merge } from "./inferredProps/RadioButton"
 
 const style: React.CSSProperties = {
-  width: "100%",
-  height: "100%"
-};
+    width: "100%",
+    height: "100%",
+}
 
 export function RadioButton(props) {
-  return <System.RadioButton {...props} style={style} />;
+    const {
+        checked,
+        color,
+        disabled,
+        onChange,
+        indeterminate,
+        label,
+        ["children"]: _,
+        ...rest
+    } = props
+
+    const [state, setState] = React.useState({ checked })
+
+    React.useEffect(() => {
+        setState({ checked })
+    }, [checked])
+
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        onChange && onChange(event)
+        setState({ ...state, checked: event.target.checked })
+    }
+
+    return (
+        <System.RadioButton
+            {...rest}
+            style={style}
+            checked={state.checked}
+            onChange={handleChange}
+            disabled={props.disabled}
+            label={props.label}
+        />
+    )
 }
 
 RadioButton.defaultProps = {
-  width: 150,
-  height: 50
-};
+    width: 100,
+    height: 24,
+    disabled: false,
+    checked: false,
+    label: "Hello World",
+}
 
 addPropertyControls(RadioButton, {
-  label: merge(controls.label, {}),
-  placeholder: merge(controls.placeholder, {})
-});
+    label: merge(controls.label, {}),
+    checked: merge(controls.checked, {}),
+    disabled: merge(controls.disabled, {}),
+    reverse: merge(controls.reverse, {}),
+})
